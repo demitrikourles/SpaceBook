d9c45fce8eb97aa4215f3668a48f0f7a77ac3f51
zainchowdhary <zain-chowdhary@hotmail.com>
2018-11-22 22:17:06 -0700

    Scripts

---
+++ db/InsertFacilities.sql
@@ -0,0 +1,19 @@
+  insert into [SpaceBook].[dbo].[Facility]
+  (name, active)
+  values ('Realtors East', 1)
+
+   insert into [SpaceBook].[dbo].[Facility]
+  (name, active)
+  values ('Realtors West', 1)
+
+   insert into [SpaceBook].[dbo].[Facility]
+  (name, active)
+  values ('Mount Pleasant North', 1)
+
+   insert into [SpaceBook].[dbo].[Facility]
+  (name, active)
+  values ('Mosaic Stadium', 1)
+
+     insert into [SpaceBook].[dbo].[Facility]
+  (name, active)
+  values ('Leibel Field', 1)
