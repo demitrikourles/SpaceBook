a9bd4ef5f250dda88c9c7f69a22ab5cfd9fb15c6
connor-meredith <connor.meredith1@gmail.com>
2019-02-07 20:30:03 -0600

    Updated facility fields
    Since I added more fields to the facility table, the model will also have these additional fields

--- SpaceBook/Models/Facility.cs
+++ SpaceBook/Models/Facility.cs
@@ -30,6 +30,12 @@ namespace SpaceBook.Models
         public Nullable<System.TimeSpan> EndTime { get; set; }
         public Nullable<decimal> HourlyRate { get; set; }
         public Nullable<bool> ActiveFlag { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Booking> Bookings { get; set; }
