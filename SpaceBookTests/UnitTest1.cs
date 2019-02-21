babab83fa8a3922a8d78e71a65e6fd9845b2a30e
connor-meredith <connor.meredith1@gmail.com>
2019-02-21 15:07:13 -0600

    Added first test and MVC package

---
+++ SpaceBookTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SpaceBookTests
+{
+    //[TestClass]
+    //public class HomeControllerTest
+    //{
+    //    [TestMethod]
+    //    public void TestIndexView()
+    //    {
+    //        var controller = new SpaceBook.HomeController();
+    //        var result = controller.Details(2) as ViewResult;
+    //        Assert.AreEqual("Details", result.ViewName);
+
+    //    }
+    //}
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //    }
+    //}
+}
